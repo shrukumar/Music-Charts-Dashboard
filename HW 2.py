193347da60fc57283c7ebf7aa8340c5839797959
melinayg <yang.meli@northeastern.edu>
2023-10-12 17:48:22 -0400

    imports

--- HW 2.py
+++ HW 2.py
@@ -1,3 +1,15 @@
-print("hi")
+from dash import Dash, dcc, html, Input, Output
+import plotly.graph_objects as go
+import plotly.express as px
+import random as rnd
+from collections import Counter
+import pandas as pd
+
 
 # https://www.kaggle.com/datasets/nelgiriyewithana/top-spotify-songs-2023
+
+def main():
+
+    # step 1: define the app object
+    app = Dash(__name__)
+
