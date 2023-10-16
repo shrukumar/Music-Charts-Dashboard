88cd958dd49e540f307fb43a94de6a49302afe87
Shru Kumar <shrutika.skumar@gmail.com>
2023-10-16 15:13:27 -0400

    dash for other visualizations

--- Vis 1.py
+++ Vis 1.py
@@ -1,53 +1,59 @@
 from dash import Dash, dcc, html, Input, Output
-import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
+
 
 # step 1: define the app object
 app = Dash(__name__)
 
-def main():
 
 # step 2: define the layout
-    app.layout = html.Div([
-        html.H4('Songs in Playlists vs Song Rankings'),
-        html.P('Select Streaming Platform:'),
-        dcc.Dropdown(
-            id="dropdown",
-            options=['apple', 'shazam', 'deezer','spotify'],
-            value='apple',
-            clearable=False
-        ),
-        dcc.Graph(id="graph")
-
-    ])
+app.layout = html.Div([
+   html.H4('Songs in Playlists vs Song Rankings'),
+   html.P('Select Streaming Platform:'),
+   dcc.Dropdown(
+       id="dropdown",
+       options=['apple', 'deezer','spotify'],
+       value='apple',
+       clearable=False
+   ),
+   dcc.Graph(id="graph")
 
 
-    # step 3: Define the callback which triggers visualization updates in response to changing the control settings
-
-    @app.callback(
-        Output("graph", "figure"),
-        Input("dropdown", "value")
-    )
+])
 
 
-    def rank_vis(platform):
-        """
-        Creates a scatter plot of the number of songs in playlists and how they
-         rank in the charts based on the platform input
-        :param platform: string that indicates platform
-        :return: plot
-        """
-        df = pd.read_csv('/Users/mel/Desktop/DS3500/spotify-2023.csv')
-        plot = px.scatter(df, x=f'in_{platform}_playlists', y=f'in_{platform}_charts', hover_data=['track_name'])
 
-        plot.update_layout(title=f'{platform} Playlists vs Rankings', xaxis_title='Number of Playlists',
-                            yaxis_title='Rankings', hovermode='closest')
-
-        plot.show()
+# step 3: Define the callback which triggers visualization updates in response to changing the control settings
 
 
-    # step 4: Run the server
-    app.run_server(debug=True)
+@app.callback(
+    Output("graph", "figure"),
+    Input("dropdown", "value")
+)
 
-main()
+
+
+
+def rank_vis(platform):
+    """
+    Creates a scatter plot of the number of songs in playlists and how they
+    rank in the charts based on the platform input
+    :param platform: string that indicates platform
+    :return: plot
+    """
+    df = pd.read_csv('spotify-2023.csv')
+    plot = px.scatter(df, x=f'in_{platform}_playlists', y=f'in_{platform}_charts', hover_data=['track_name'])
+
+
+    plot.update_layout(title=f'{platform} Playlists vs Rankings', xaxis_title='Number of Playlists',
+                       yaxis_title='Rankings', hovermode='closest')
+
+
+    return plot
+
+
+
+
+# step 4: Run the server
+app.run_server(debug=True)
--- Vis 3.py
+++ Vis 3.py
@@ -1,42 +1,84 @@
-import matplotlib.pyplot as plt
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
 import pandas as pd
 
+
+# step 1: define the app object
+app = Dash(__name__)
+
+
+# step 2: define the layout
+app.layout = html.Div([
+   html.H4('Check Song Rankings'),
+   html.P('Enter Song Name :'),
+
+    # dcc.Dropdown(
+    #     id="dropdown",
+    #     options=['Die For You', 'Lucid Dreams', 'Rush'],
+    #     value='Rush',
+    #     clearable=False
+    dcc.Input(
+        id='input',
+        placeholder='Enter text...',
+        type='text'
+
+   ),
+
+
+   dcc.Graph(id="graph")
+
+
+])
+
+
+@app.callback(
+   Output("graph", "figure"),
+   Input("input", "value")
+)
 def input_song(name):
 
-    df = pd.read_csv('/Users/mel/Desktop/DS3500/spotify-2023.csv')
+
+   df = pd.read_csv('spotify-2023.csv')
 
 
-    keys = df['track_name'].values
-    keys_list = list(keys)
-
-    if name in keys_list:
-
-        filtered_df = df.loc[df['track_name'] == f'{name}']
-
-        # Get the row index associated with the key in the 'Name' column
-        row_index = filtered_df.index[0]
+   keys = df['track_name'].values
+   keys_list = list(keys)
 
 
-        song_dict = {'Spotify': df.iloc[row_index]['in_spotify_charts'],
-                     'Apple' :df.iloc[row_index]['in_apple_charts'],
-                     'Deezer' :df.iloc[row_index]['in_deezer_charts'],
-                     'Shazam' :df.iloc[row_index]['in_shazam_charts'] }
-
-        # change data types of chart values into integers
-        int_values = [int(value) for value in song_dict.values()]
-
-        platforms = list(song_dict.keys())
-
-        charts = int_values
-
-        # creation of bar plot
-        plt.bar(platforms, charts)
-        plt.title('Rankings of Streaming Services')
-        plt.xlabel('Platforms')
-        plt.ylabel('Charts')
-
-        plt.tight_layout()
-        plt.show()
+   if name in keys_list:
 
 
-input_song("Die For You")
+       filtered_df = df.loc[df['track_name'] == f'{name}']
+
+
+       # Get the row index associated with the key in the 'Name' column
+       row_index = filtered_df.index[0]
+
+
+
+
+       song_dict = {'Spotify': df.iloc[row_index]['in_spotify_charts'],
+                    'Apple' :df.iloc[row_index]['in_apple_charts'],
+                    'Deezer' :df.iloc[row_index]['in_deezer_charts'],
+                    'Shazam' :df.iloc[row_index]['in_shazam_charts'] }
+
+
+       # change data types of chart values into integers
+       int_values = [int(value) for value in song_dict.values()]
+
+
+       platforms = list(song_dict.keys())
+
+
+       charts = int_values
+
+
+       # creation of bar plot
+       fig = px.bar(x=platforms, y=charts)
+       fig.update_layout(title = 'Rankings of Streaming Services', xaxis_title='Platforms', yaxis_title='Charts')
+
+       return fig
+
+
+# step 4: Run the server
+app.run_server(debug=True)
